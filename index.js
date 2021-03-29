6709c95d39be812e145947560a414e47444f335e
parulagg-1199 <68728482+parulagg-1199@users.noreply.github.com>
2021-03-29 16:31:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
 const name = "Parul Aggarwal";
-const currDate = new Date().toLocaleDateString();
-const currTime = new Date().toLocaleTimeString();
+const img1 = "Parul Aggarwal";
+const img2 = "Parul Aggarwal";
+const img3 = "Parul Aggarwal";
+
 
 ReactDOM.render(
   <>
-    <h1> My name is {name} </h1>
-    <p> Current date is {currDate}</p>
-    <p> Current time is {currTime}</p>
+    <h1 className="heading" >JSX Attributes and CSS Styling</h1>
+    <h4 id="name">My name is {name}</h4>
+    <div className="img_div">
+      <img src="https://picsum.photos/200/300" alt="Random image" />
+      <img src="https://picsum.photos/250/300" alt="Random image" />
+      <img src="https://picsum.photos/300/300" alt="Random image" />
+    </div>
   </>,
   document.getElementById('root')
 );
