8d2eb53ba22a5d355ef6d92483216c5b1adaf96d
parulagg-1199 <68728482+parulagg-1199@users.noreply.github.com>
2021-04-05 12:12:24 +0530

    using  components

---
+++ react/Component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Heading from './Heading';
+import Paragraph from './Paragraph';
+import List from './List';
+
+
+ReactDOM.render(
+   <>
+   <Heading />
+   <Paragraph />
+   <List />
+   </>,
+  document.getElementById('root')
+);
+
+
